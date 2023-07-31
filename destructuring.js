4f8d8e94452736ec1dbce55dcd3d8c6e931d4df5
Lee Curtis <leec313@gmail.com>
2023-07-31 17:39:29 +0000

    initial commit

--- destructuring.js
+++ destructuring.js
@@ -4,9 +4,52 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+let { mike, jill, alicia } = jobs;
+console.log(mike, jill, alicia);
 
 // Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let langauges2 = {
+    first: "english",
+    second: "french",
+    third: "german",
+    fourth: "japanese",
+};
+let { first, third } = langauges2;
+console.log(first, third);
+
 
 // Using rest parameter syntax
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favorite, secondFavorite, ...others] = fruits;
+console.log(favorite);
+console.log(secondFavorite);
+console.log(others);
+
+let favefoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "veggy",
+    andrea: "steak",
+};
+let { brian, anna, ...rest } = favefoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
+
